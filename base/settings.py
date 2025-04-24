6f8d9e108975d7897c88e288b5c270f98775c04d
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-04-24 22:36:42 +0500

    i added postgresqli data base

--- base/settings.py
+++ base/settings.py
@@ -63,8 +63,11 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'PORT': '5432',
     }
 }
 
