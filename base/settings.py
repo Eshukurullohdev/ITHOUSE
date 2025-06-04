467411c361f60564d780609723816627d88c5e56
Eshukurullohdev <shukurullohergashev2@gmail.com>
2025-06-04 13:04:41 +0500

    sql

--- base/settings.py
+++ base/settings.py
@@ -62,10 +62,10 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-import dj_database_url
 DATABASES = {
     'default': {
-        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
